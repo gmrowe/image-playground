f31565af6de1aa3d86b3ee7fff25fd0a9355aa16
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-01-17 11:13:20 -0700

    renamed some variables to make algorithm a little more readable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-//! An example of opening an image.
 use std::path::Path;
 
 const BG_SOURCE: &str = "./assets/background.png";
@@ -9,18 +8,20 @@ fn alpha_blend(bg: &image::Rgba<u8>, fg: &image::Rgba<u8>) -> image::Rgba<u8> {
     let image::Rgba([r0, g0, b0, a0]) = *bg;
     let image::Rgba([r1, g1, b1, a1]) = *fg;
 
-    let a0_f = a0 as f64 / 255.0;
-    let a1_f = a1 as f64 / 255.0;
-    let a_comp_f = a1_f + a0_f * (1.0 - a1_f);
+    let bg_alpha = a0 as f64 / 255.0;
+    let fg_alpha = a1 as f64 / 255.0 / 2.0;
+    let comp_alpha = fg_alpha + bg_alpha * (1.0 - fg_alpha);
+    
+    let a_blend = |bg_comp, fg_comp| {
+        let raw =
+            (fg_comp as f64 * fg_alpha) + (bg_comp as f64 * bg_alpha) * (1.0 - fg_alpha);
+        (raw / comp_alpha).floor() as u8
+    };
 
-    let cr = ((r1 as f64 * a1_f) + (r0 as f64 * a0_f) * (1.0 - a1_f)) / a_comp_f;
-    let cg = ((g1 as f64 * a1_f) + (g0 as f64 * a0_f) * (1.0 - a1_f)) / a_comp_f;
-    let cb = ((b1 as f64 * a1_f) + (b0 as f64 * a0_f) * (1.0 - a1_f)) / a_comp_f;
-      
-    image::Rgba([cr.floor() as u8,
-                 cg.floor() as u8,
-                 cb.floor() as u8,
-                 (a_comp_f * 255.0).floor() as u8
+    image::Rgba([a_blend(r0, r1),
+                 a_blend(g0, g1),
+                 a_blend(b0, b1),
+                 (comp_alpha * 255.0).floor() as u8
     ])
 }
 
